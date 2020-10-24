b1c09ca0662b51ca8bae6149c4be380920f5070c
Лямин <genrih.md@gmail.com>
2020-10-24 16:55:09 +0300

    Rem old lines

--- tests/addonTests.js
+++ tests/addonTests.js
@@ -2,9 +2,8 @@ var addon = require("bindings")("express-go");
 var assert = require("assert");
 var calculations = 10000000;
 
-describe("addon", function() {
+describe("express-go", function() {
     it("should calculate pi synchronously", function() {
-        var result = addon.calculateSync(calculations);
-        assert.equal(Math.round(result * 1000), 3142);
+        
     });
 });

cb0aaff9a608ceb5c9756561c85624dd36418eba
Лямин <genrih.md@gmail.com>
2020-11-13 15:54:03 +0300

    Restor prev ver of main

--- src/maIn.go
+++ src/maIn.go
@@ -3,6 +3,8 @@ package main
 import (
 	"reflect"
 	"unsafe"
+
+	"github.com/napi-bindings/go-napi-sys/napisys"
 )
 
 /*
@@ -14,37 +16,25 @@ import (
 */
 import "C"
 
-// NapiValue is binding to C.napi_value
-type NapiValue = C.napi_value
-
-// NapiEnv is binding to C.napi_env
-type NapiEnv = C.napi_env
-
-// NapiStatus is binding to C.napi_status
-type NapiStatus = C.napi_status
-
-// NapiCallbackInfo is binding to C.napi_callback_info
-type NapiCallbackInfo = C.napi_callback_info
-
 //export Method
-func Method(env C.napi_env, info C.napi_callback_info) C.napi_value {
-	var res C.napi_value
-	C.napi_create_int32(env, C.int(5), &res)
+func Method(env napisys.Env, info napisys.CallbackInfo) napisys.Value {
+	var res napisys.Value
+	napisys.CreateInt32(env, C.int(5), &res)
 	return res
 }
 
 //export Init
-func Init(env C.napi_env, exports C.napi_value) C.napi_value {
+func Init(env Env, exports Value) napisys.Value {
 	f := reflect.ValueOf(Method).Pointer()
 	// var status NapiStatus
-	desc := C.napi_property_descriptor{
+	desc := napisys.PropertyDescriptor{
 		C.CString("hello"),
 		nil,
-		(C.napi_callback)(unsafe.Pointer(&f)),
+		(napisys.Callback)(unsafe.Pointer(&f)),
 		nil,
 		nil,
 		nil,
-		C.napi_default,
+		napisys.Default,
 		unsafe.Pointer(nil),
 	}
 	C.napi_define_properties(env, exports, 1, &desc)
